5c13d0912af36594523ba1867ffdb13e39a53925
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-24 20:54:07 -0400

    added in navbar, props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>GitHub Info</h1>
+        <Navbar title='GitHub Central' icon='fab fa-github' />
       </div>
     );
   }
