b00392f80bcb78e79b01f8202cf359be85cb79ef
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-31 14:16:48 -0400

    added in react router, created about page component, added in home and about routes to navbar

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About this Application</h1>
+      <p>Search and pull user information from Github</p>
+      <p>Version: 0.0.4</p>
+    </div>
+  );
+};
+
+export default About;
