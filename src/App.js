51a16bd762a18fb2f48b19012c7f8bfc121fa57d
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-31 11:16:45 -0400

    set prop to text input on submit, passed props up to search query function

--- src/App.js
+++ src/App.js
@@ -11,20 +11,22 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  searchUsers = async text => {
     this.setState({ loading: true });
 
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}`
+    );
 
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar title='GitHub Central' icon='fab fa-github' />
-        <div class='container'>
-          <Search />
+        <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
